b829915233e041f1479d5a2f4bebdd1e571e4177
Yanis Riani <Yann.riani@gmail.com>
2024-01-23 04:32:59 +0100

    Clear / Formating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-sm text-zinc-500">
           Bonjour, je m'appelle Yanis. Actuellement, je travaille comme
           développeur Web FullStack pour{" "}
           <Link
