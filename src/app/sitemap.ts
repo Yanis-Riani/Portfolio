8567157ae5bfc4760dfd700e19847081c4232212
Yanis Riani <Yann.riani@gmail.com>
2024-01-28 17:17:41 +0100

    Update / Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,17 +1,33 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next"
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://yanisriani.fr',
+      url: "https://yanisriani.fr",
       lastModified: new Date(),
     },
     {
-      url: 'https://yanisriani.fr/contact',
+      url: "https://yanisriani.fr/contact",
       lastModified: new Date(),
     },
     {
-      url: 'https://yanisriani.fr/projets',
+      url: "https://yanisriani.fr/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://yanisriani.fr/resume",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://yanisriani.fr/projects/Keploid",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://yanisriani.fr/projects/Domap",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://yanisriani.fr/projects/Moteur3D",
       lastModified: new Date(),
     },
   ]
